5397b4dad895df317b5844a6e70e577535bcb8eb
Felixdov <36860070+Felixdov@users.noreply.github.com>
2019-03-21 18:56:39 +0200

    Initial Changes to master

---
+++ Rental/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rental.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
---
+++ Rental/Controllers/RentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rental.Controllers
+{
+    public class RentController : Controller
+    {
+        // GET: Rent
+        public ActionResult NewRent_ToView()
+        {
+            ViewBag.Title = "New Rental";
+            return View();
+        }
+    }
+}
